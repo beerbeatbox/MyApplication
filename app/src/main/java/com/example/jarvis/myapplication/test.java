43de29ab6801e71bf91c1627c2f027c614961f7e
Jarvis <beer.beatbox.one@gmail.com>
2017-06-12 01:32:22 +0700

    add b1

--- app/src/main/java/com/example/jarvis/myapplication/test.java
+++ app/src/main/java/com/example/jarvis/myapplication/test.java
@@ -6,15 +6,6 @@ package com.example.jarvis.myapplication;
 
 public class test {
     int a=0;
-    //add in b1
-    //add in master
-
-    //master add 1
-    //add b1
-    //add master
-    //add line3
-
-    //delete b1
 
 
 }
